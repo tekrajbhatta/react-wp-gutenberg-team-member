c3c69a3b7bde8cdd32468ba7385649e1d3d6f9c4
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-12-04 18:35:32 +0545

    Added styles and modifed the save.js file structure

--- save.js
+++ save.js
@@ -1,5 +1,6 @@
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 import ArrowIcon from './arrow_right.svg';
+import ArrowIconGreen from './arrow_right_green.svg';
 
 function save({ attributes }) {
     const { imageUrl, imageAlt, name, designation } = attributes;
@@ -22,21 +23,25 @@ function save({ attributes }) {
                             alt=""
                             className="team-member-arrow"
                         />
+                        <img
+                            src={ArrowIconGreen}
+                            alt=""
+                            className="team-member-arrow-green"
+                        />
+                        <div className="team-member-info">
+                            <RichText.Content
+                                tagName="h3"
+                                className="team-member-name"
+                                value={name}
+                            />
+                            <RichText.Content
+                                tagName="p"
+                                className="team-member-designation"
+                                value={designation}
+                            />
+                        </div>
                     </div>
-                    
                 )}
-                <div className="team-member-info">
-                    <RichText.Content
-                        tagName="h3"
-                        className="team-member-name"
-                        value={name}
-                    />
-                    <RichText.Content
-                        tagName="p"
-                        className="team-member-designation"
-                        value={designation}
-                    />
-                </div>
             </div>
         </div>
     );
